1a27a7ce8ce718a07d65e7f35934f1e8fdf21750
Kaitlyn Kwan <kyk25@cornell.edu>
2014-09-30 15:07:51 -0400

    added edit for posts and users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
 	end
 
 	def new
+		@user = User.new
 	end
 
 	def create
@@ -28,5 +29,21 @@ class UsersController < ApplicationController
 		params.require(:user).permit(:first_name, :last_name, :email, :password)
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		respond_to do |format|
+			if @user.update(user_params)
+				format.html{redirect_to @user, notice: 'User was successfully updated!'}
+				format.json{head :no_content}
+			else
+				format.html{render action:'edit'}
+				format.json{render json: @user.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 	
 end
